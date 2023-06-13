4bf1a28877d8174a9639787c624f4c7f766b450a
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-13 23:45:35 +0200

    Fixed Auth url (Auth)

--- spotify-hub-development/src/components/Authentication/AuthService.js
+++ spotify-hub-development/src/components/Authentication/AuthService.js
@@ -2,7 +2,7 @@ import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9927b119c4a7420ca10a6a881a955e6f&response_type=token&redirect_uri=https://localhost:3000&scope=comma_separated_scopes`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9927b119c4a7420ca10a6a881a955e6f&response_type=token&redirect_uri=http://localhost:3000&scope=comma_separated_scopes`;
 
 export const login = () => {
     window.location = AUTH_URL;

63514893d0b323545c135ae508f45e4d81636a2e
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-13 23:43:59 +0200

    Added hook useEffect in function App (App.js)

--- spotify-hub-development/src/App.js
+++ spotify-hub-development/src/App.js
@@ -2,13 +2,20 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { handleRedirect } from './components/Authentication/AuthService';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    handleRedirect();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Spotify Hub</h1>
+        <p>Please log in with your Spotify account to continue.</p>
+        <a href="/login">Log in with Spotify</a>
       </header>
     </div>
   );
-}
+};
 
 export default App;

bc02aa4cd063bf9ee37d80fef4b034790a2f23c3
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-13 23:36:42 +0200

    Added functions and button (Login.js)

--- spotify-hub-development/src/pages/Login.js
+++ spotify-hub-development/src/pages/Login.js
@@ -1,2 +1,16 @@
-import { Loging } from '../components/Authentication/AuthService';
+import { Loging, login } from '../components/Authentication/AuthService';
 
+const LoginPage = () => {
+    const handleLogin = () => {
+        login();
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <button onClick={handleLogin}>Log in with Spotify</button>
+        </div>
+    );
+};
+
+export default LoginPage;

c9fa9341e4cad5f0f8544ea202219cff8d14eee5
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-14 08:29:37 +0200

    Modify  Auth so it doesn't use route (Auth)

--- spotify-hub-development/src/components/Authentication/AuthService.js
+++ spotify-hub-development/src/components/Authentication/AuthService.js
@@ -4,23 +4,14 @@ const spotifyApi = new SpotifyWebApi();
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9927b119c4a7420ca10a6a881a955e6f&response_type=token&redirect_uri=http://localhost:3000/&scope=comma_separated_scopes`;
 
-export const login = () => {
-    window.location = AUTH_URL;
-  };
+export const handleLogin = () => {
+    window.location.href = AUTH_URL;
+ };
 
 export const handleRedirect = () => {
-    const { access_token } = getUrlParams(window.location.hash);
-    if(access_token) {
-        spotifyApi.setAccessToken(access_token);
-    }
-};
-
-const getUrlParams = (hash) => {
-    const params = {};
-    const hashParams = hash.slice(1).split('&');
-    for (let param of hashParams) {
-        const [key, value] = param.split('=');
-        params[key] = value;
-    }
-    return params;
+    const url = new URL(window.location.href);
+    const accessToken = url.searchParams.get('access_token');
+    if (accessToken) {
+        spotifyApi.setAccessToken(accessToken);
+    } 
 };

7e031be9cf185478f3e970e564b1691787a0bddd
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-14 09:07:05 +0200

    Added a simple hub page and modified AUTH URL

--- spotify-hub-development/src/components/Authentication/AuthService.js
+++ spotify-hub-development/src/components/Authentication/AuthService.js
@@ -2,7 +2,7 @@ import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=9927b119c4a7420ca10a6a881a955e6f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=9927b119c4a7420ca10a6a881a955e6f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state&state=YOUR_STATE`;
 
 export const handleLogin = () => {
     window.location.href = AUTH_URL;
---
+++ spotify-hub-development/src/pages/Hub.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HubPage = () => {
+  return (
+    <div>
+      <h1>Hub Page</h1>
+      {/* Add your hub page content here */}
+    </div>
+  );
+};
+
+export default HubPage;

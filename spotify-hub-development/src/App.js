83332f5a17c92336c8f3cfdee7199a27e0739d5c
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-14 08:25:01 +0200

    Modify App.js so it doesn't use route dom (App.js)

--- spotify-hub-development/src/App.js
+++ spotify-hub-development/src/App.js
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import LoginPage from './pages/Login';
-import { handleRedirect } from './components/Authentication/AuthService';
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState('login');
+
   useEffect(() => {
     handleRedirect();
   }, []);
 
+  const handleRedirect = () => {
+    const url = new URL(window.location.href);
+    if (url.searchParams.get('access_token')) {
+      setCurrentPage('hub');
+    }
+  };
+
   return (
-    <Router>
-      <div className='App'>
-        <header className='App-header'>
-          <h1>Spotify Hub</h1>
-        </header>
-        <Switch>
-          <Route exact path='/login' component={LoginPage} />
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Spotify Hub</h1>
+      </header>
+      {currentPage === 'login' && <LoginPage />}
+      {currentPage === 'hub' && <div>Welcome to the Hub!</div>}
+    </div>
   );
 };
 

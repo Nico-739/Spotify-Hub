73ad5c1ba40c3f556e0e13ffba483cd748858550
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 09:12:10 +0200

    Added Function to see if follow Artist (Profile)

--- spotify-hub-development/src/components/Profile/ProfileService.js
+++ spotify-hub-development/src/components/Profile/ProfileService.js
@@ -16,30 +16,43 @@ const getProfileInfo = async (accessToken) => {
 };
 
 const processProfileInfo = (profileData) => {
-  
-    return {
-      country: profileData.country,
-      display_name: profileData.display_name,
-      email: profileData.email,
-      explicit_content: {
-        filter_enabled: profileData.explicit_content.filter_enabled,
-        filter_locked: profileData.explicit_content.filter_locked,
-      },
-      external_urls: {
-        spotify: profileData.external_urls.spotify,
-      },
-      followers: {
-        href: profileData.followers.href,
-        total: profileData.followers.total,
-      },
-      id: profileData.id,
-      images: profileData.images,
-      product: profileData.product,
-      type: profileData.type,
-      uri: profileData.uri,
-    };
+  return {
+    country: profileData.country,
+    display_name: profileData.display_name,
+    email: profileData.email,
+    explicit_content: {
+      filter_enabled: profileData.explicit_content.filter_enabled,
+      filter_locked: profileData.explicit_content.filter_locked,
+    },
+    external_urls: {
+      spotify: profileData.external_urls.spotify,
+    },
+    followers: {
+      href: profileData.followers.href,
+      total: profileData.followers.total,
+    },
+    id: profileData.id,
+    images: profileData.images,
+    product: profileData.product,
+    type: profileData.type,
+    uri: profileData.uri,
   };
-  
+};
+
+const checkFollowingStatus = async (accessToken, type, ids) => {
+  try {
+    const response = await axios.get(`https://api.spotify.com/v1/me/following/contains?type=${type}&ids=${ids.join(',')}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error checking following status:', error);
+    throw error;
+  }
+};
 
 export const fetchAndProcessProfileInfo = async (accessToken) => {
   try {
@@ -48,6 +61,16 @@ export const fetchAndProcessProfileInfo = async (accessToken) => {
     return processedData;
   } catch (error) {
     console.error('Error fetching and processing profile info:', error);
+    throw error;
+  }
+};
+
+export const fetchFollowingStatus = async (accessToken, type, ids) => {
+  try {
+    const followingStatus = await checkFollowingStatus(accessToken, type, ids);
+    return followingStatus;
+  } catch (error) {
+    console.error('Error fetching following status:', error);
     throw error;
   }
 };

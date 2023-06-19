7abdf0529904509ae4b94e3ae9faba8d8a81892f
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 10:29:45 +0200

    Added function from Tracks (Hub.js)

--- spotify-hub-development/src/pages/Hub.js
+++ spotify-hub-development/src/pages/Hub.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { fetchAndProcessProfileInfo, fetchFollowingStatus } from '../components/Profile/ProfileService';
+import { getSavedTracks } from '../components/Tracks/TracksService';
 
 const HubPage = () => {
   const [profileInfo, setProfileInfo] = useState(null);
   const [followingStatus, setFollowingStatus] = useState(null);
+  const [savedTracks, setSavedTracks] = useState(null);
 
   useEffect(() => {
     const fetchProfileInfo = async () => {
@@ -11,12 +13,14 @@ const HubPage = () => {
         const accessToken = localStorage.getItem('accessToken');
         const processedData = await fetchAndProcessProfileInfo(accessToken);
         setProfileInfo(processedData);
-        const status = await fetchFollowingStatus(accessToken, 'artist', ['74ASZWbe4lXaubB36ztrGX', '08td7MxkoHQkXnWAYD8d6Q']); // Example artist IDs
+        const status = await fetchFollowingStatus(accessToken, 'artist', ['74ASZWbe4lXaubB36ztrGX', '08td7MxkoHQkXnWAYD8d6Q']);
         setFollowingStatus(status);
+        const tracks = await getSavedTracks(accessToken, 'US'); // Add a valid market code, e.g., 'US'
+        setSavedTracks(tracks);
       } catch (error) {
         console.error('Error fetching and processing profile info:', error);
       }
-    };
+    };    
 
     fetchProfileInfo();
   }, []);
@@ -26,7 +30,7 @@ const HubPage = () => {
       <h1>Hub Page Test</h1>
       {profileInfo ? (
         <div>
-          <h2>Welcome, {profileInfo.display_name}!</h2>
+          <h2>Welcome, {profileInfo.display_name}</h2>
           <p>Email: {profileInfo.email}</p>
           <p>Country: {profileInfo.country}</p>
           <p>Followers: {profileInfo.followers.total}</p>
@@ -39,6 +43,20 @@ const HubPage = () => {
         </div>
       ) : (
         <div>Loading profile information...</div>
+      )}
+      {savedTracks ? (
+        <div>
+          <h3>Your Saved Tracks:</h3>
+          <ul>
+            {savedTracks.items.map((item) => (
+              <li key={item.track.id}>
+                {item.track.name} by {item.track.artists.map((artist) => artist.name).join(', ')}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>Loading saved tracks...</div>
       )}
     </div>
   );

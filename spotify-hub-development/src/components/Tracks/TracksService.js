7257711941b5da20beef1ee21f58d2512690f8ee
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 10:53:49 +0200

    Added function to display Albums (Tracks)

--- spotify-hub-development/src/components/Tracks/TracksService.js
+++ spotify-hub-development/src/components/Tracks/TracksService.js
@@ -13,7 +13,32 @@ export const getSavedTracks = async (accessToken, market = '', limit = 20, offse
       },
     });
 
-    return response.data;
+    const tracks = response.data.items;
+    const trackIds = tracks.map((track) => track.track.id);
+
+    // Fetch album information for all tracks in parallel
+    const albumResponses = await Promise.all(
+      trackIds.map((trackId) =>
+        axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+      )
+    );
+
+    const albums = albumResponses.map((albumResponse) => albumResponse.data.album);
+
+    // Merge album information with the tracks
+    const tracksWithAlbums = tracks.map((track, index) => ({
+      ...track,
+      album: albums[index],
+    }));
+
+    return {
+      ...response.data,
+      items: tracksWithAlbums,
+    };
   } catch (error) {
     console.error('Error fetching saved tracks:', error);
     throw error;

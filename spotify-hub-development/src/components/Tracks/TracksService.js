523f3267bbe4e8c1d42eb2f60bc3b9c23c2d454c
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 15:59:54 +0200

    Added refresh token logic (Tracks)

--- spotify-hub-development/src/components/Tracks/TracksService.js
+++ spotify-hub-development/src/components/Tracks/TracksService.js
@@ -1,22 +1,50 @@
 import axios from 'axios';
 
-export const getSavedTracks = async (accessToken, market = '', limit = 20, offset = 0) => {
+const refreshAccessToken = async () => {
+  const refreshToken = localStorage.getItem('refreshToken');
+  const clientId = '9927b119c4a7420ca10a6a881a955e6f';
+  const clientSecret = '8776f50ee48b485780b87bce3bffca19';
+
+  const params = new URLSearchParams();
+  params.append('grant_type', 'refresh_token');
+  params.append('refresh_token', refreshToken);
+  params.append('client_id', clientId);
+  params.append('client_secret', clientSecret);
+
   try {
-    const response = await axios.get(`https://api.spotify.com/v1/me/tracks`, {
+    const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    const { access_token } = response.data;
+    localStorage.setItem('accessToken', access_token);
+
+    return access_token;
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+    throw error;
+  }
+};
+
+export const getSavedTracks = async () => {
+  try {
+    let accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      accessToken = await refreshAccessToken();
+    }
+
+    const response = await axios.get('https://api.spotify.com/v1/me/tracks', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        market,
-        limit,
-        offset,
       },
     });
 
     const tracks = response.data.items;
     const trackIds = tracks.map((track) => track.track.id);
 
-    // Fetch album information for all tracks in parallel
     const albumResponses = await Promise.all(
       trackIds.map((trackId) =>
         axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
@@ -29,7 +57,6 @@ export const getSavedTracks = async (accessToken, market = '', limit = 20, offse
 
     const albums = albumResponses.map((albumResponse) => albumResponse.data.album);
 
-    // Merge album information with the tracks
     const tracksWithAlbums = tracks.map((track, index) => ({
       ...track,
       album: albums[index],

02b7801cd463a001ced8167246af0456644ec472
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 14:29:43 +0200

    Updated so it works with hub (Playlist)

--- spotify-hub-development/src/components/Playlist/PlaylistService.js
+++ spotify-hub-development/src/components/Playlist/PlaylistService.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
-export const getUserPlaylists = async (accessToken, playlistId, market = '') => {
+export const getUserPlaylists = async (accessToken) => {
   try {
-    const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+    const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-      params: {
-        market,
-      },
     });
 
-    return response.data;
+    return response.data.items;
   } catch (error) {
-    console.error('Error fetching playlist:', error);
+    console.error('Error fetching user playlists:', error);
     throw error;
   }
 };

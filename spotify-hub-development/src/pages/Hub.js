db54a370c783278d21eea56476ef2ff670da18da
Nicolaj <nicolajhaueter72@gmail.com>
2023-06-19 14:29:16 +0200

    Updated so better display (Hub.js)

--- spotify-hub-development/src/pages/Hub.js
+++ spotify-hub-development/src/pages/Hub.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { fetchAndProcessProfileInfo, fetchFollowingStatus } from '../components/Profile/ProfileService';
 import { getSavedTracks } from '../components/Tracks/TracksService';
+import { getUserPlaylists } from '../components/Playlist/PlaylistService';
 
 const HubPage = () => {
   const [profileInfo, setProfileInfo] = useState(null);
   const [followingStatus, setFollowingStatus] = useState(null);
   const [savedTracks, setSavedTracks] = useState(null);
+  const [userPlaylists, setUserPlaylists] = useState(null);
 
   useEffect(() => {
     const fetchProfileData = async () => {
       try {
         const accessToken = localStorage.getItem('accessToken');
 
-        const processedProfileInfo = await fetchAndProcessProfileInfo(accessToken);
-        setProfileInfo(processedProfileInfo);
+        const processedData = await fetchAndProcessProfileInfo(accessToken);
+        setProfileInfo(processedData);
 
-        const followingStatus = await fetchFollowingStatus(accessToken, 'artist', ['74ASZWbe4lXaubB36ztrGX', '08td7MxkoHQkXnWAYD8d6Q']);
-        setFollowingStatus(followingStatus);
+        const status = await fetchFollowingStatus(accessToken, 'artist', ['74ASZWbe4lXaubB36ztrGX', '08td7MxkoHQkXnWAYD8d6Q']);
+        setFollowingStatus(status);
 
-        const savedTracks = await getSavedTracks(accessToken, 'US');
-        setSavedTracks(savedTracks);
+        const tracks = await getSavedTracks(accessToken, 'US');
+        setSavedTracks(tracks);
+
+        const playlists = await getUserPlaylists(accessToken);
+        setUserPlaylists(playlists);
       } catch (error) {
         console.error('Error fetching and processing profile info:', error);
       }
@@ -28,49 +33,24 @@ const HubPage = () => {
     fetchProfileData();
   }, []);
 
-  const renderProfileInfo = () => {
-    if (!profileInfo) {
-      return <div>Loading profile information...</div>;
+  const renderPlaylists = () => {
+    if (!userPlaylists) {
+      return <div>Loading playlists...</div>;
     }
 
-    const { display_name, email, country, followers, images } = profileInfo;
-
-    return (
-      <div>
-        <h2>Welcome, {display_name}</h2>
-        <p>Email: {email}</p>
-        <p>Country: {country}</p>
-        <p>Followers: {followers.total}</p>
-        <p>Following Artists: {followingStatus ? 'Yes' : 'No'}</p>
-        {images.length > 0 && (
-          <p>
-            Profile Image: <img src={images[0].url} alt="Profile" />
-          </p>
-        )}
-      </div>
-    );
-  };
-
-  const renderSavedTracks = () => {
-    if (!savedTracks) {
-      return <div>Loading saved tracks...</div>;
+    if (userPlaylists.length === 0) {
+      return <div>No playlists found.</div>;
     }
 
     return (
       <div>
-        <h3>Your Saved Tracks:</h3>
+        <h3>Your Playlists:</h3>
         <ul>
-          {savedTracks.items.map((item) => (
-            <li key={item.track.id}>
-              <div>
-                <img src={item.track.album.images[0].url} alt="Album" />
-              </div>
-              <div>
-                <p>{item.track.name}</p>
-                <p>
-                  {item.track.artists.map((artist) => artist.name).join(', ')} - {item.track.album.name}
-                </p>
-              </div>
+          {userPlaylists.map((playlist) => (
+            <li key={playlist.id}>
+              <p>Name: {playlist.name}</p>
+              <p>Owner: {playlist.owner.display_name}</p>
+              <p>Total Tracks: {playlist.tracks.total}</p>
             </li>
           ))}
         </ul>
@@ -81,8 +61,47 @@ const HubPage = () => {
   return (
     <div>
       <h1>Hub Page Test</h1>
-      {renderProfileInfo()}
-      {renderSavedTracks()}
+      {profileInfo ? (
+        <div>
+          <h2>Welcome, {profileInfo.display_name}</h2>
+          <p>Email: {profileInfo.email}</p>
+          <p>Country: {profileInfo.country}</p>
+          <p>Followers: {profileInfo.followers.total}</p>
+          <p>Following Artists: {followingStatus ? 'Yes' : 'No'}</p>
+          {profileInfo.images.length > 0 && (
+            <p>
+              Profile Image: <img src={profileInfo.images[0].url} alt="Profile" />
+            </p>
+          )}
+        </div>
+      ) : (
+        <div>Loading profile information...</div>
+      )}
+
+      {savedTracks ? (
+        <div>
+          <h3>Your Saved Tracks:</h3>
+          <ul>
+            {savedTracks.items.map((item) => (
+              <li key={item.track.id}>
+                <div>
+                  <img src={item.track.album.images[0].url} alt="Album" />
+                </div>
+                <div>
+                  <p>{item.track.name}</p>
+                  <p>
+                    {item.track.artists.map((artist) => artist.name).join(', ')} - {item.track.album.name}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>Loading saved tracks...</div>
+      )}
+
+      {renderPlaylists()}
     </div>
   );
 };
